07fafed343fa8e09e01a3e24ea8ebfc3dd0346d6
Henrik Gjestang <henrik.logj@gmail.com>
2021-11-22 10:51:14 +0100

    update theme to new version, and set theme-path to repo

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,7 +2,7 @@
 # Oh-My-Posh #
 ##############
 Import-Module oh-my-posh
-Set-PoshPrompt -Theme powerlevel10k_rainbow
+Set-PoshPrompt -Theme ~\dev\dotfiles-windows\pk10_custom_theme.omp.json
 # Remove prompt in history
 Enable-PoshTransientPrompt
 
@@ -41,45 +41,45 @@ Set-Alias juno "jupyter notebook"
 Set-Alias jula "jupyter lab"
 
 # Git
-function Get-GitStatus { & git status -sb $args } 
-New-Alias -Name gst -Value Get-GitStatus -Force -Option AllScope 
-function Get-GitDiff { & git diff $args } 
-New-Alias -Name gdi -Value Get-GitDiff -Force -Option AllScope 
-function Get-GitPush { & git push --follow-tags $args } 
-New-Alias -Name gpu -Value Get-GitPush -Force -Option AllScope 
-function Get-GitFetch { & git fetch $args } 
-New-Alias -Name gfe -Value Get-GitFetch -Force -Option AllScope 
-function Get-GitPull { & git pull $args } 
-New-Alias -Name gpl -Value Get-GitPull -Force -Option AllScope 
-function Get-GitCheckout { & git checkout $args } 
-New-Alias -Name gch -Value Get-GitCheckout -Force -Option AllScope 
-function Get-GitCommit { & git commit -m $args } 
-New-Alias -Name gco -Value Get-GitCommit -Force -Option AllScope 
-function Get-GitAdd { & git add -- $args } 
-New-Alias -Name gad -Value Get-GitAdd -Force -Option AllScope 
-function Get-GitRm { & git rm $args } 
-New-Alias -Name grm -Value Get-GitRm -Force -Option AllScope 
-function Get-GitMv { & git mv $args } 
-New-Alias -Name gmv -Value Get-GitMv -Force -Option AllScope 
-function Get-GitLog { & git log $args } 
-New-Alias -Name glo -Value Get-GitLog -Force -Option AllScope 
-function Get-GitTree { & git log --graph --oneline --decorate $args } 
-New-Alias -Name gtr -Value Get-GitTree -Force -Option AllScope 
-#function Get-GitCherryPick { & git cherry-pick $args } 
+function Get-GitStatus { & git status -sb $args }
+New-Alias -Name gst -Value Get-GitStatus -Force -Option AllScope
+function Get-GitDiff { & git diff $args }
+New-Alias -Name gdi -Value Get-GitDiff -Force -Option AllScope
+function Get-GitPush { & git push --follow-tags $args }
+New-Alias -Name gpu -Value Get-GitPush -Force -Option AllScope
+function Get-GitFetch { & git fetch $args }
+New-Alias -Name gfe -Value Get-GitFetch -Force -Option AllScope
+function Get-GitPull { & git pull $args }
+New-Alias -Name gpl -Value Get-GitPull -Force -Option AllScope
+function Get-GitCheckout { & git checkout $args }
+New-Alias -Name gch -Value Get-GitCheckout -Force -Option AllScope
+function Get-GitCommit { & git commit -m $args }
+New-Alias -Name gco -Value Get-GitCommit -Force -Option AllScope
+function Get-GitAdd { & git add -- $args }
+New-Alias -Name gad -Value Get-GitAdd -Force -Option AllScope
+function Get-GitRm { & git rm $args }
+New-Alias -Name grm -Value Get-GitRm -Force -Option AllScope
+function Get-GitMv { & git mv $args }
+New-Alias -Name gmv -Value Get-GitMv -Force -Option AllScope
+function Get-GitLog { & git log $args }
+New-Alias -Name glo -Value Get-GitLog -Force -Option AllScope
+function Get-GitTree { & git log --graph --oneline --decorate $args }
+New-Alias -Name gtr -Value Get-GitTree -Force -Option AllScope
+#function Get-GitCherryPick { & git cherry-pick $args }
 #New-Alias -Name gcp -Value Get-GitCherryPick -Force -Option AllScope
-#function Get-GitCommitEdit { & git commit -ev $args } 
-#New-Alias -Name gce -Value Get-GitCommitEdit -Force -Option AllScope 
-#function Get-GitCommitAmend { & git commit --amend $args } 
-#New-Alias -Name gca -Value Get-GitCommitAmend -Force -Option AllScope 
-#function Get-GitAddAll { & git add --all $args } 
-#New-Alias -Name gaa -Value Get-GitAddAll -Force -Option AllScope 
-#function Get-GitCheckoutBranch { & git checkout -b $args } 
-#New-Alias -Name gchb -Value Get-GitCheckoutBranch -Force -Option AllScope 
-#function Get-GitBranch { & git branch $args } 
-#New-Alias -Name gb -Value Get-GitBranch -Force -Option AllScope 
-#function Get-GitRemoteView { & git remote -v $args } 
+#function Get-GitCommitEdit { & git commit -ev $args }
+#New-Alias -Name gce -Value Get-GitCommitEdit -Force -Option AllScope
+#function Get-GitCommitAmend { & git commit --amend $args }
+#New-Alias -Name gca -Value Get-GitCommitAmend -Force -Option AllScope
+#function Get-GitAddAll { & git add --all $args }
+#New-Alias -Name gaa -Value Get-GitAddAll -Force -Option AllScope
+#function Get-GitCheckoutBranch { & git checkout -b $args }
+#New-Alias -Name gchb -Value Get-GitCheckoutBranch -Force -Option AllScope
+#function Get-GitBranch { & git branch $args }
+#New-Alias -Name gb -Value Get-GitBranch -Force -Option AllScope
+#function Get-GitRemoteView { & git remote -v $args }
 #New-Alias -Name grv -Value Get-GitRemoteView -Force -Option AllScope
-#function Get-GitRemoteAdd { & git remote add $args } 
+#function Get-GitRemoteAdd { & git remote add $args }
 #New-Alias -Name gra -Value Get-GitRemoteAdd -Force -Option AllScope
 
 
