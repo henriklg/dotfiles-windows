29998a351b806c3327efca61906752b171a217d8
Henrik Gjestang <henrik.logj@gmail.com>
2024-11-22 12:07:44 +0100

    Added untested code for symlink of terminal settings, and point $profile to powershell_profile.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -3,7 +3,29 @@
 # and reload profile.
 
 # Flow:
+# - Ask user for confirmation
 # - Grab origin main from repo
 # - Copy to correct direcotries
 # - Source profile etc
+#   - add sourcing of pwsh profile in local profile.ps1 (look in top of powershell_profile.ps1) and run "reload"
+#   - add symlink to terminal settings-file
+#   - add symlink to gitconfig-file
+#   - (maybe?) add symlink to vscode_usersettings-file
 # ___
+
+
+
+# _____________________________________________________
+# Symlink Windows Terminal Settings to dotfiles-windows
+# _____________________________________________________
+Remove-Item -Path $Env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState -Force –Recurse
+# need admin to symlink
+New-Item -ItemType SymbolicLink -Path "$Env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" -Target "$HOME\dev\dotfiles-windows\terminal_setup"
+
+
+# ______________________________________
+# Point pwsh profile to dotfiles-windows
+# ______________________________________
+# NB: must be run from pwsh 7
+# NB: first check if expression already in $PROFILE
+Set-Content -Path $PROFILE -Value '". $HOME\dev\dotfiles-windows\powershell_profile.ps1" | Invoke-Expression'
