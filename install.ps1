d50731cc66bb1623ceb017851d0e8e4c400a0fbb
Henrik Gjestang <henrik.logj@gmail.com>
2025-09-02 16:14:03 +0200

    Refactor install script to enhance tool installation and update process

--- install.ps1
+++ install.ps1
@@ -1,56 +1,67 @@
+
 # _____________________________________________________
-# Script to set up and install dependencies like pyenv,
-# poetry, terminal, powershell 7, Oh-my-posh
-
-# NB: Script must be run as a administrative shell
+# Script to set up and install/update dependencies for data science dev
+# Usage: Run as administrator. Use --update to upgrade tools.
 # _____________________________________________________
 
-# to run script, set execution policy with PS-admin:
-Set-ExecutionPolicy AllSigned
+param(
+    [switch]$Update
+)
 
-# ______________
-# Winget install
-# ______________
-winget install Microsoft.PowerShell (msstore)
-winget install Microsoft.WindowsTerminal (msstore)
-winget install JanDeDobbeleer.OhMyPosh (msstore)
-winget install Git.Git
-winget install Microsoft.VisualStudioCode
-winget install astral-sh.uv
-winget install astral-sh.ruff
-# docker, gsudo, notepad++, python, wsl?
+# Set execution policy (if not already set)
+if ((Get-ExecutionPolicy) -ne 'AllSigned') {
+    Set-ExecutionPolicy AllSigned -Scope Process -Force
+}
 
-# ______________
+# List of tools to install/update via winget
+$wingetTools = @(
+    @{ Id = 'Microsoft.PowerShell'; Source = 'msstore' },
+    @{ Id = 'Microsoft.WindowsTerminal'; Source = 'msstore' },
+    @{ Id = 'JanDeDobbeleer.OhMyPosh'; Source = 'msstore' },
+    @{ Id = 'Git.Git'; Source = 'winget' },
+    @{ Id = 'Microsoft.VisualStudioCode'; Source = 'winget' },
+    @{ Id = 'astral-sh.uv'; Source = 'winget' },
+    @{ Id = 'astral-sh.ruff'; Source = 'winget' }
+    # Add more tools as needed
+)
+
+function Install-Or-Update-Tool {
+    param(
+        [string]$Id,
+        [string]$Source
+    )
+    if ($Update) {
+        Write-Host "Updating $Id..."
+        winget upgrade --id $Id --source $Source --silent --accept-package-agreements --accept-source-agreements
+    } else {
+        Write-Host "Installing $Id..."
+        winget install $Id -s $Source --silent --accept-package-agreements --accept-source-agreements
+    }
+}
+
+# Install or update all tools
+foreach ($tool in $wingetTools) {
+    Install-Or-Update-Tool -Id $tool.Id -Source $tool.Source
+}
+
 # Other installs
-# ______________
 # Nerd fonts (from fonts-dir in repo, or 'oh-my-posh font install meslo')
-# Terminal-Icons (done automatically from profile.ps1, or manually with 'Install-Module - Name Terminal-Icons')
+# Terminal-Icons (done automatically from profile.ps1, or manually with 'Install-Module -Name Terminal-Icons')
 # Pyenv? (disable windows app aliases)
 # Poetry? ('poetry config virtualenvs.in-project true' to store venv in project folder)
 
+# Python env setup
+Write-Host "Installing pyenv-win..."
+Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"
+&"./install-pyenv-win.ps1"
 
-# __________
-# Python env
-# __________
-# Pyenv
-Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"; &"./install-pyenv-win.ps1"
-
-# Poetry - NB: needs python installed
+Write-Host "Installing Poetry..."
 (Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | py -
 #poetry config virtualenvs.in-project true
 
-
-# _____________
 # Install fonts
-# _____________
 Install-Fonts
 
-
-# _________
-# Functions
-# _________
-
-# NB: not tested!
 function Install-Fonts {
     $FONTS = 0x14
     $Path = ".\fonts"
@@ -62,7 +73,6 @@ function Install-Fonts {
             $try = $true
             $installedFonts = @(Get-ChildItem c:\windows\fonts | Where-Object {$_.PSIsContainer -eq $false} | Select-Object basename)
             $name = $File.baseName
-
             foreach($font in $installedFonts) {
                 $font = $font -replace "_", ""
                 $name = $name -replace "_", ""
